ddaebba1046ea3b3309d5c347825d0e3328dabfa
Doohan AS <79241754+doohanas@users.noreply.github.com>
2021-07-09 11:02:24 +0700

    Menambahkan asynchronous function untuk menampilkan data music library dari API dengan format JSON

--- module-1/season-2/homeWork-2/assets/js/app.js
+++ module-1/season-2/homeWork-2/assets/js/app.js
@@ -3,3 +3,22 @@ const btnSubmit = document.getElementById("btn-sbmt");
 btnSubmit.addEventListener("click", () => {
     alert("Done");
 });
+
+// Get a file from API in JSON Format
+async function getFile() {
+  let myPromise = new Promise((myResolve, myReject) => {
+    let req = new XMLHttpRequest();
+    req.open('GET', "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json");
+    req.onload = () => {
+      if (req.status == 200) {
+        myResolve(req.response);
+      } else {
+        myReject("File not Found");
+      }
+    };
+    req.send();
+  });
+  console.log(await myPromise);
+}
+
+getFile();
