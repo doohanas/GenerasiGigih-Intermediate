8cfd48096cb284a47ecd800202b9bde5731bf319
Doohan AS <79241754+doohanas@users.noreply.github.com>
2021-07-10 20:50:26 +0700

    Menampilkan data artis di halaman web & Mengambil data JSON dari API

--- module-1/season-2/homeWork-2/assets/js/app.js
+++ module-1/season-2/homeWork-2/assets/js/app.js
@@ -4,21 +4,13 @@ btnSubmit.addEventListener("click", () => {
     alert("Done");
 });
 
-// Get a file from API in JSON Format
-async function getFile() {
-  let myPromise = new Promise((myResolve, myReject) => {
-    let req = new XMLHttpRequest();
-    req.open('GET', "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json");
-    req.onload = () => {
-      if (req.status == 200) {
-        myResolve(req.response);
-      } else {
-        myReject("File not Found");
-      }
-    };
-    req.send();
+axios.get('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json', {})
+  .then(response => {   
+    console.log(response.data);
+    const title = document.getElementById("box-description-title").innerHTML= response.data.name;
+    const artist = document.getElementById("box-description-artist").innerHTML= response.data.album.artists[0].name;
+    const album = document.getElementById("box-description-album").innerHTML= response.data.album.name;
+  })
+  .catch((err) => {
+    console.log(err.data);
   });
-  console.log(await myPromise);
-}
-
-getFile();
